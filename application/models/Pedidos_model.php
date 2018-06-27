9a721a3e14f98a7c95d593cc88995f9ca999b002
Matheus <matheusnarciso@hotmail.com>
2018-06-27 17:42:47 -0300

    modelo

---
+++ application/models/Pedidos_model.php
@@ -0,0 +1,31 @@
+<?php
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+ 
+class Pedidos_model extends CI_Model {
+
+    public function  __construct() {
+    }
+	
+	public function getQtdAbertoFechado() {
+        $sql = "SELECT 
+                  SUM(a.fechado) fechado,
+                  SUM(a.aberto) aberto 
+                FROM
+                  (SELECT 
+                    COUNT(1) fechado,
+                    0 aberto 
+                  FROM
+                    tbl_pedido 
+                  WHERE situacao = 'e' 
+                  UNION
+                  ALL 
+                  SELECT 
+                    0 fechado,
+                    COUNT(1) aberto 
+                  FROM
+                    tbl_pedido 
+                  WHERE situacao <> 'e') a " ;
+        $query = $this->db->query($sql);
+        return $query->result_object()[0];
+    }
+}
